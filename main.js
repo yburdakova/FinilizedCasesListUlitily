673277d66e08479e3feb6a345f2370e304613174
yburdakova <bu.asti@gmail.com>
2025-03-28 10:50:26 -0400

    refactor: migrate project to ES Modules

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+import { app, BrowserWindow, ipcMain, dialog } from 'electron';
+import path, { join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: join(__dirname, 'preload.js')
     }
   });
 
@@ -20,6 +24,18 @@ app.whenReady().then(() => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
 });
+
+ipcMain.handle('dialog:selectFolder', async () => {
+    const result = await dialog.showOpenDialog({
+      properties: ['openDirectory']
+    });
+  
+    if (result.canceled || result.filePaths.length === 0) {
+      return null;
+    }
+  
+    return result.filePaths[0];
+  });
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
